8179de84b5d7c312b844fa2a2cb50e4f4d7b43b0
Nick M <nrm170630@utdallas.edu>
2019-04-09 22:30:29 -0500

    Changed addCard method to always insert new card at index 0.

--- Project/src/Pile.java
+++ Project/src/Pile.java
@@ -1,32 +1,33 @@
-import java.util.ArrayList;
+import java.util.Stack;
 
 public class Pile {
 
     // attributes for Pile class
-    ArrayList<Card> discardPile = new ArrayList<>(52);
+    public Stack<Card> discardPile = new Stack<>();
 
     // Constructor
     public Pile(Card starter){
-        discardPile.add(starter);
+        discardPile.push(starter);
     }
 
     // Returns top card of discard pile
     public Card getTopCard(){
-        Card card = discardPile.get(discardPile.size());
+        Card card = discardPile.peek();
         return card;
     }
 
     // Adds card to top of discard pile
     public void addCard(Card c){
         Card topCard = getTopCard();
-        if(c.getValue() == "8"){
-            discardPile.add(c);
+        if(c.getValue().equals("8")){
+            discardPile.push(c);
         }
-        else if(c.getSuit() == topCard.getSuit() || c.getValue() == getTopCard().getValue()){
-            discardPile.add(c);
+        else if(c.getSuit().equals(topCard.getSuit()) || c.getValue().equals(getTopCard().getValue())){
+            discardPile.push(c);
         }
         else{   //Card is not playable
             System.out.println("Card is not playable. Please make another choice.");
+            // TODO Add method to draw a card from the deck
         }
     }
 }
