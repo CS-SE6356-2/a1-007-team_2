7e2ec1a0850c123f9bc60e086f5d60b355017673
Nick M <nrm170630@utdallas.edu>
2019-04-16 11:11:39 -0500

    Update Player.java
    Added method to calculate the score of a players hand.

--- Project/src/Player.java
+++ Project/src/Player.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Player {
 
@@ -19,6 +21,28 @@ public class Player {
     // Get player name
     public String getPlayerName() {
         return playerName;
+    }
+
+    // Get players hand score
+    public int getHandScore(){
+        int score = 0;
+        List numbers = Arrays.asList("2", "3", "4", "5", "6", "7", "9", "10");
+        List royal = Arrays.asList("A", "K", "Q", "J");
+        for (Card card:playerHand){
+            String value = card.getValue();
+            if (numbers.contains(value)){
+                score = score + Integer.parseInt(value);
+            } else if (royal.contains(value)){
+                if (value.equals("A")){
+                    score = score + 1;
+                } else {
+                    score = score + 10;
+                }
+            } else {
+                score = score + 50;
+            }
+        }
+        return score;
     }
 
     // Prints cards in the Players hand
