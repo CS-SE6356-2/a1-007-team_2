a08420e5d67c6f248ae7536026d6cf2692033e02
Nick M <nrm170630@utdallas.edu>
2019-04-28 19:46:07 -0500

    Updated CrazyEights class to index the player in order to show in the GUI where the card is played from. Also updated methods to resolve issues with the the game.

--- Project/src/main/java/CrazyEights.java
+++ Project/src/main/java/CrazyEights.java
@@ -1,10 +1,5 @@
-import javafx.scene.layout.GridPane;
-
 import javax.swing.*;
-import java.awt.*;
 import java.util.ArrayList;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
 public class CrazyEights {
 
@@ -16,10 +11,19 @@ public class CrazyEights {
         // Start a Single Player Game
         public ArrayList<Player> startLocalGame(String playerName) {
             ArrayList<Player> playerList = new ArrayList<>(4);
-            playerList.add(new Player(playerName));
-            playerList.add(new Player("Two"));
-            playerList.add(new Player("Three"));
-            playerList.add(new Player("Four"));
+            Player one = new Player(playerName);
+            one.setPlayerIndex(1);
+            Player two = new Player("Two");
+            two.setPlayerIndex(2);
+            Player three = new Player("Three");
+            three.setPlayerIndex(3);
+            Player four = new Player("Four");
+            four.setPlayerIndex(4);
+
+            playerList.add(one);
+            playerList.add(two);
+            playerList.add(three);
+            playerList.add(four);
             return playerList;
     }
 
@@ -34,7 +38,7 @@ public class CrazyEights {
     // Creates a Discard Pile to be used during the game and updates the UI
     public Pile stockPile(Deck deck, Controller controller){
         Pile stockPile = new Pile(deck.deal());
-        controller.updatePile(stockPile.getTopCard());
+        controller.updatePile(stockPile.getTopCard(), 0);
         this.chosenSuit = stockPile.getTopCard().getSuit();
         return stockPile;
     }
@@ -42,17 +46,20 @@ public class CrazyEights {
     // Adds a playable card to the Pile and updates the player hand and UI
     private void playCard(Player player, Card playCard, Pile pile, Controller controller){
         pile.addCard(playCard);
-        controller.updatePile(playCard);
+        controller.updatePile(playCard, player.getPlayerIndex());
         player.getHand().remove(playCard);
         controller.updatePlayerCards();
     }
 
     // Checks for a winner at the end of each players turn
-    private void checkForWinner(Player player){
-        if (player.getHand().size() == 0){
-            JFrame frame = new JFrame();
-            JOptionPane.showMessageDialog(frame, "Player "+player.getPlayerName()+" WINS!");
-            System.exit(0);
+    private void checkForWinner(ArrayList<Player> players){
+        for (Player p:players) {
+            if (p.getHand().size() == 0) {
+                JFrame frame = new JFrame();
+                JOptionPane.showMessageDialog(frame, "Player " + p.getPlayerName() + " WINS!");
+                calculateScores(players);
+                break;
+            }
         }
     }
 
@@ -62,27 +69,20 @@ public class CrazyEights {
         int i = 0;
         String[] choices = {"Diamond", "Spade", "Club", "Heart", "2", "3", "4", "5", "6", "7", "8", "9", "10",
                 "Jack", "Queen", "King", "Ace"};
-
-
         JFrame frame = new JFrame();
         String result = JOptionPane.showInputDialog(frame, "Enter selected suit or value: \n i.e. King, Club, 5, " +
                 "Spade, Ace, 10");
 
         // determine suit to play based on option selected
         // then add a dummy card to the pile
-
         for (String selection: choices){
             if (result.equals(selection)) {
                 card = card.dummyCard(i);
             }
             i++;
         }
-
         pile.addCard(card);
         controller.playersChoice(card);
-
-        //If the return value was null/empty.
-        //setLabel("Please choose a suit.");
     }
 
     // If the Deck is empty at the end of the players turn method calculateScore()
@@ -94,7 +94,7 @@ public class CrazyEights {
             scoreMessage = scoreMessage.concat("Player " + player.getPlayerName() +
                     " Score:" + player.getHandScore() + "\n");
         }
-        JOptionPane.showMessageDialog(frame, scoreMessage);
+        JOptionPane.showMessageDialog(frame, scoreMessage, "Player Scores", JOptionPane.PLAIN_MESSAGE,null);
     }
 
     // When a card is picked from the UI the turnPlay() method is called
@@ -102,20 +102,17 @@ public class CrazyEights {
     public void turnPlay(int cardIndex, ArrayList<Player> players, Deck deck, Pile pile, Controller controller) {
         Player player = players.get(0);
         Card playCard = player.getHand().get(cardIndex);
-
         if (playCard.getValue().equals("8")){
             playCard(player, playCard, pile, controller);
             playEight(pile, controller);
             AIPlay(players, deck, pile, controller);
-        } else if(playCard.getSuit().equals(pile.getTopCard().getSuit()) || playCard.getValue().equals(pile.getTopCard().getValue())){
+        } else if(playCard.getSuit().equals(pile.getTopCard().getSuit()) ||
+                playCard.getValue().equals(pile.getTopCard().getValue())){
             playCard(player, playCard, pile, controller);
             AIPlay(players, deck, pile, controller);
         } else {
             JOptionPane.showMessageDialog(null, "Card is not Playable. Please Choose Another Card.");
         }
-
-        checkForWinner(player);
-
     }
 
     private boolean isPlayable(Card c, Pile top){
@@ -124,32 +121,24 @@ public class CrazyEights {
         }
         return false;
     }
-
+    
     // At the end of the user's turn the method AIPlay() is called to update
     // the pile if the AI player has a playable card then update the UI
-    private void AIPlay(ArrayList<Player> players, Deck deck, Pile stockPile, Controller controller){
-        Card topPile = stockPile.getTopCard();
-        int playableCard = 0;
-        boolean hasPlayable = false;
-
-        // loop through AI players cards to see if they
+    public void AIPlay(ArrayList<Player> players, Deck deck, Pile stockPile, Controller controller){
+         // loop through AI players cards to see if they
         // have a card that can be played on the pile
-        for (int i = 1; i < 3; i++){
-            // Pause to mimic an AI player making their choice; 5-10 seconds
-//            Random rand = new Random();
-//            int sleepTime = rand.nextInt(5);
-//            try {
-//                TimeUnit.SECONDS.sleep(2);
-//            } catch(InterruptedException e) {
-//                System.out.println("Error! Player "+i+" was interrupted!");
-//            }
-
-
+        for (int i = 1; i < 4; i++){
+            int playableCard = 0;
+            boolean hasPlayable = false;
             ArrayList<Card> playerCards = players.get(i).getHand();
+            Card topPile = stockPile.getTopCard();
             for (Card c : playerCards){
-                if (c.getSuit().equals(topPile.getSuit())||c.getValue().equals(topPile.getValue())) {
+                if (c.getSuit().equals(topPile.getSuit())||
+                        c.getValue().equals(topPile.getValue())||
+                        c.getValue().equals("8")) {
                     hasPlayable = true;
                     playableCard = players.get(i).getHand().indexOf(c);
+                    break;
                 } else {
                     hasPlayable = false;
                 }
@@ -158,21 +147,16 @@ public class CrazyEights {
             // if a playable exists in the players hand
             // the Card is added to the Pile and the UI is updated
             if (hasPlayable){
-                controller.updatePile(players.get(i).getHand().get(playableCard));
+                controller.updatePile(players.get(i).getHand().get(playableCard), players.get(i).getPlayerIndex());
                 stockPile.addCard(players.get(i).getHand().get(playableCard));
                 players.get(i).getHand().remove(playableCard);
             } else {
 
                 if (deck.getCardList().size() > 0){
                     players.get(i).getHand().add(deck.deal());
-
-                } else {
-                    calculateScores(players);
                 }
             }
-
-            checkForWinner(players.get(i));
-
         }
+        checkForWinner(players);
     }
 }
