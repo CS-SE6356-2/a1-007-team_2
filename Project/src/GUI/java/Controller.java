c315ef3d3d0c606e9935e8b6e969f7d89993758d
Nick M <nrm170630@utdallas.edu>
2019-04-28 19:13:00 -0500

    Updated Controller class by adding animations to the cards when each player is adding a card to the Pile. Also added methods to display the rules of the game and group contact information for the corresponding menu options.

--- Project/src/GUI/java/Controller.java
+++ Project/src/GUI/java/Controller.java
@@ -1,3 +1,4 @@
+import javafx.animation.PathTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
@@ -5,10 +6,10 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
-
+import javafx.scene.shape.Line;
+import javafx.util.Duration;
 import javax.swing.*;
 import java.util.ArrayList;
-
 
 public class Controller{
 
@@ -46,6 +47,37 @@ public class Controller{
         String input = JOptionPane.showInputDialog(frame, "Enter your name:");
         playerOneName.setText(input);
         gameType();
+    }
+
+    // display the game rules when the user selects the About Crazy Eights option from the menu
+    @FXML private void rules(){
+        JOptionPane.showMessageDialog(null, "Object of the Game\n" +
+                "The goal is to be the first player to get rid of all the cards in their hand.\n\n" +
+                "Card Values/Scoring\n" +
+                "The player who is the first to have no cards left wins the game. \nThe winning player collects from " +
+                "each other player the value of the cards remaining in that player’s hand as follows:\n" +
+                "Each eight = 50 points\n" +
+                "Each K, Q, J or 10 = 10 points\n" +
+                "Each ace = 1 point\n" +
+                "Each other card is the face value\n\n" +
+                "The Play\n" +
+                "Starting with player one, the next player must place one card face up on the starter pile.\n" +
+                "Each card played (other than an eight) must match the card showing on the starter pile, either in " +
+                "suit or in value.\n" +
+                "If unable to play, the player draws one card from the deck and the turn is passed to the next player.\n"+
+                "All eights are wild. That is, an eight may be played at any time in turn, and the player need only " +
+                "specify a suit or value. \nThe next player must play either a card of the specified suit, value, or " +
+                "an eight.", "Crazy Eight Rules", JOptionPane.PLAIN_MESSAGE, null);
+    }
+
+    @FXML private void contact(){
+        JLabel message = new JLabel("<html><center>If there are any issues while using this program "+
+                "please contact the members of<br>Group 2 from SE 3345.007 - Spring 2019:<br><br>" +
+                "Nicholas Malzac: nrm170630@UTDallas.edu<br>" +
+                "Martha Bridges: mmb160330@UTDallas.edu<br>" +
+                "Valeria Grimaldo: vag170000@UTDallas.edu<br>" +
+                "Dylan Capece: dsc160330@UTDallas.edu");
+        JOptionPane.showMessageDialog(null, message, "Contact Information", JOptionPane.PLAIN_MESSAGE, null );
     }
 
     // User selects the type of game to play
@@ -130,15 +162,69 @@ public class Controller{
         } else {
             players.get(0).getHand().add(deck.deal());
             updatePlayerCards();
+            game.AIPlay(players, deck, pile, this);
         }
     }
 
     // Updates the the UI with the most recent card played
-    public void updatePile(Card card){
-        String playCard = card.getValue()+card.getSuit().substring(0,1);
-        String imagePath = String.valueOf(getClass().getResource("images/cards/" + playCard + ".png"));
-        Image pic = new Image(imagePath);
-        pileImg.setImage(pic);
+    public void updatePile(Card card, int playerIndex){
+        String cardBack = String.valueOf(getClass().getResource("images/cards/back5.png"));
+        Image cardBackImg = new Image(cardBack);
+        ImageView cardFromHand = new ImageView();
+        cardFromHand.setFitHeight(158);
+        cardFromHand.setFitWidth(120);
+        cardFromHand.setImage(cardBackImg);
+        cardFromHand.toFront();
+        int x = 0, y = 0;
+        int gridRow = 0, gridCol = 0;
+        int timer = 0;
+        switch(playerIndex){
+            case 1:
+                x = -89;
+                y = -178;
+                gridRow = 4;
+                gridCol = 4;
+                timer = 1;
+                break;
+            case 2:
+                x = 570;
+                y = -63;
+                gridRow = 3;
+                gridCol = 0;
+                timer = 2;
+                break;
+            case 3:
+                x = 245;
+                y = 346;
+                gridRow = 0;
+                gridCol = 2;
+                timer = 3;
+                break;
+            case 4:
+                x = -250;
+                y = 220;
+                gridRow = 1;
+                gridCol = 5;
+                timer = 4;
+                break;
+        }
+        Line line = new Line(0,0,x,y);
+        mainGrid.add(cardFromHand,gridCol,gridRow);
+        PathTransition path = new PathTransition(Duration.seconds(timer), line);
+        path.setNode(cardFromHand);
+        path.setOnFinished(e -> {
+            String playCard = card.getValue()+card.getSuit().substring(0,1);
+            String imagePath = String.valueOf(getClass().getResource("images/cards/" + playCard + ".png"));
+            Image pic = new Image(imagePath);
+            pileImg.setImage(pic);
+            cardFromHand.setVisible(false);
+        });
+        path.setAutoReverse(false);
+        if (card.getValue().equals("8") & playerIndex == 1){
+            cardFromHand.setVisible(false);
+        } else {
+            path.play();
+        }
     }
 
     // Updates the the UI with the most recent card played
@@ -153,4 +239,5 @@ public class Controller{
         Image pic = new Image(imagePath);
         pileImg.setImage(pic);
     }
+
 }
